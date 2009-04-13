ea6cc58ad7974eb5f4d079f1c08f56257e336125
antonio <atd@lapiluka.org>
2009-04-13 19:44:49 +0200

    More detailed Network Contents in Home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,10 @@ class HomeController < ApplicationController
   authentication_filter
 
   def index
-    @contents = ActiveRecord::Content.all(:container => current_agent.stages,
-                                                  :page => params[:page],
-                                                  :per_page => 10,
-                                                  :select => "id, title, created_at, updated_at, owner_id, owner_type")
+    @contents = 
+      ActiveRecord::Content.all(:container => Array(current_agent) + current_agent.stages,
+                                :page => params[:page],
+                                :per_page => 10,
+                                :select => "id, title, created_at, updated_at, owner_id, owner_type, author_id, author_type")
   end
 end
