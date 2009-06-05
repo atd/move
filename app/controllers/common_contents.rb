6666cf140f5ac92c6d929be1e3426d7bce536d15
antonio <atd@lapiluka.org>
2009-06-05 15:47:05 +0200

    Fix deprecation warnings

--- app/controllers/common_contents.rb
+++ app/controllers/common_contents.rb
@@ -2,9 +2,9 @@ module CommonContents
   class << self
     def included(base)
       base.class_eval do
-        before_filter :container!, :except => [ :index, :show ]
+        before_filter :current_container!, :except => [ :index, :show ]
         authorization_filter :read, :resource, :only => [ :show ]
-        authorization_filter [ :create, :content ], :container, :only => [ :new, :create ]
+        authorization_filter [ :create, :content ], :current_container, :only => [ :new, :create ]
         authorization_filter :update, :resource, :only => [ :edit, :update ]
         authorization_filter :delete, :resource, :only => [ :destroy ]
 
@@ -14,7 +14,7 @@ module CommonContents
   end
 
   def index_with_public
-    @conditions = ( container && authorized?([ :read, :content], container) ?
+    @conditions = ( current_container && authorized?([ :read, :content], current_container) ?
                    nil :
                    { :public_read => true } )
       
