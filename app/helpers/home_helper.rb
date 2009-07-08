8dd5982a057e6b3e72aac2a2cd2a7bc8bd6194f3
antonio <atd@lapiluka.org>
2009-07-08 16:32:37 +0200

    Group titles in Public and Home

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,6 +1,7 @@
 module HomeHelper
   def home_sidebar
     returning "" do |html|
+      html << "<h1>#{ t('user.group.other') }</h1>"
       html << '<div id="home-sidebar">'
       html << render(:partial => 'groups', :object => Array(current_agent) + current_agent.stages)
       html << '<div id="new-group" class="actions span-6">'
--- app/helpers/public_helper.rb
+++ app/helpers/public_helper.rb
@@ -1,6 +1,7 @@
 module PublicHelper
   def public_menu
     returning "" do |html|
+      html << "<h1>#{ t('group.other') }</h1>"
       html << '<div id="public_groups" class="groups span-7 last">'
       html << render(:partial => 'group', :collection => Group.all)
       html << '</div>'
