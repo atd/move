1e5619397814106bde16b9ebc19a6d2c170fd6c8
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2009-10-27 14:58:56 +0100

    Station sync

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -5,7 +5,6 @@ class UserObserver < ActiveRecord::Observer
 
   def after_save(user)
     if user.class.password_recovery?
-      UserMailer.deliver_activation(user) if user.recently_activated?
       UserMailer.deliver_lost_password(user) if user.recently_lost_password?
       UserMailer.deliver_reset_password(user) if user.recently_reset_password?
     end
