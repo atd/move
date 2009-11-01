16087260996e9b4090a3b62b4eb575a56e5535b5
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2009-11-01 14:19:38 +0100

    Rearrange home#sidebar

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,13 +1,2 @@
 module HomeHelper
-  def home_sidebar
-    returning "" do |html|
-      html << "<h1>#{ t('user.group.other') }</h1>"
-      html << '<div id="home-sidebar">'
-      html << render(:partial => 'groups', :object => Array(current_agent) + current_agent.stages)
-      html << '<div id="new-group" class="actions span-6">'
-      html << link_to("#{ logo(Group.new) } #{  t('group.new') }", new_group_path)
-      html << '</div>'
-      html << '</div>'
-    end
-  end
 end
