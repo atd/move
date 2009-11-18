54fdd19e320bc0a0af03138eae646f60edf528df
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2009-11-18 12:24:57 +0100

    Fix TagsController#show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,7 +5,7 @@ class TagsController
   def show_with_authorization
     show_without_authorization
 
-    @taggables.delete_if{ |t| !t.authorize?(:read, :to => current_agent) }
+    @taggables = @taggables.select{ |t| t.authorize?(:read, :to => current_agent) }
   end
 
   alias_method_chain :show, :authorization
