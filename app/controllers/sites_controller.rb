10a122b591622160ed918076c7d5e9795bf31fb5
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-02-05 12:13:01 +0100

    Fix deprecated method site

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,5 +2,5 @@
 require_dependency "#{ RAILS_ROOT }/vendor/plugins/station/app/controllers/sites_controller"
 
 class SitesController
-  authorization_filter :update, :site
+  authorization_filter :update, :current_site
 end
